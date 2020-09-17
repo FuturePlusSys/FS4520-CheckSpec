585f525569cdb90fe733a35c298e16df513bddd0
FuturePlus Systems <Barb.Aichinger@FuturePlus.com>
2020-09-17 11:59:44 -0400

    Add files via upload

---
+++ DP14ValidationAttributes/DP14ValidationAttributes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// Validation tests attributes
+/// </summary>
+///
+///https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/attributes/accessing-attributes-by-using-reflection
+///
+namespace DP14ValidationAttributes_ClassLibrary
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]  // Multiuse attribute.
+
+    public class DP14ValidationAttributes : System.Attribute
+    {
+        public string CategoryID { get; set; } = "Testing";
+
+        public string ProtocolID { get; set; } = "SST";
+
+        public int ListOrder { get; set; } = 0;
+
+        public DP14ValidationAttributes(string catID, string pID, int orderID)
+        {
+            CategoryID = catID;
+            ProtocolID = pID;
+            ListOrder = orderID;
+        }
+    }
+}
---
+++ DP14ValidationTestsInterface/IFS4500_VTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace DP14ValidationTestsInterface
+{
+    public interface IFS4500_VTests
+    {
+        List<string> GetTestDescriptions();  
+
+        void Initialize(int testID, List<string> configParameters, int maxReportedErrors = 10);
+
+        List<string> doTest(int testNumber, byte[] stateData);
+
+        List<string> GetTestResultsSummary(int testNumber);
+
+        List<string> GetTestResultsDetailed(int testNumber);
+
+        List<string> GetTestOverview(int testNumber);
+    }
+}
